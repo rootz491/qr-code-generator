0ba2b635980ef46f2019849aa90f5fdadc973311
nikhilswain <nikhilswain36@gmail.com>
2022-10-31 22:39:13 +0530

    root page forma and jobcard component

--- jobcard/frontend/src/pages/portal.js
+++ jobcard/frontend/src/pages/portal.js
@@ -1,15 +1,25 @@
-import { Typography } from "@mui/material";
+import { Stack, Box } from "@mui/material";
+import FormComponent from "../component/FormComponents";
+import JobCardComponent from "../component/JobCardComponent";
 
 function Portal() {
   return (
     <>
-      <Typography variant="h1" color="pink" textAlign="center">
+      {/* <Typography variant="h1" color="pink" textAlign="center">
         Hello World
       </Typography>
       <Typography variant="body1" color="red" textAlign="center">
         {" "}
         left side form, right side live preview Jobcard
-      </Typography>
+      </Typography> */}
+      <Stack direction="row" sx={{ height: "100vh" }}>
+        <Stack sx={{ width: "50%", height: "100%", overflowY: "scroll" }} alignItems="center">
+          <FormComponent />
+        </Stack>
+        <Stack sx={{ flex: 1 }} justifyContent="center" alignItems="center">
+          <JobCardComponent />
+        </Stack>
+      </Stack>
     </>
   );
 }
