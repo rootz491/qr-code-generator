a2145fdf77dd2140f76420694ef5237c3c5491f7
unknown <anadithakur99@gmail.com>
2022-11-01 13:44:24 +0530

    jobcard layout change started

--- jobcard/frontend/src/component/JobCardComponent/CardComponents/CardHeader.js
+++ jobcard/frontend/src/component/JobCardComponent/CardComponents/CardHeader.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Typography } from "@mui/material";
 
-const CardHeader = ({name}) => {
+const cardHeaderStyles = makeStyles({
+  cardHeader: {
+    width: "100%",
+    height: "45px",
+    backgroundColor: "#ffffff",
+  },
+  name: {
+    padding: "12px 12px",
+    fontSize: "20px",
+    lineHeight: "24px",
+    color: "#000000",
+  },
+});
+
+const CardHeader = ({ name }) => {
+  const classes = cardHeaderStyles();
   return (
-    <div className="cardHeader">
-      <p className="name">{name}</p>
+    <div className={classes.cardHeader}>
+      <Typography fontWeight={700} className={classes.name}>{name}</Typography>
     </div>
   );
 };
--- jobcard/frontend/src/component/JobCardComponent/index.js
+++ jobcard/frontend/src/component/JobCardComponent/index.js
@@ -1,26 +1,68 @@
-import "./styles.css";
+// import "./styles.css";
 import candidate from "./demo.json";
-import divider from "../../icons/line.svg";
+// import divider from "../../icons/line.svg";
 import CardHeader from "./CardComponents/CardHeader";
 import ExperienceDiv from "./CardComponents/ExperienceDiv";
 import ContactDiv from "./CardComponents/ContactDiv";
 import SalaryDiv from "./CardComponents/SalaryDiv";
-import MessageDiv from './CardComponents/MessageDiv';
+import MessageDiv from "./CardComponents/MessageDiv";
+import { Card, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const jobCardStyles = makeStyles({
+  jobCard: {
+    position: "absolute",
+    margin: "5%",
+    padding: "0",
+    width: "300px",
+    height: "420px",
+    backgroundColor: "#efefef !important",
+    // borderRadius: "10px",
+
+    // border: "1px solid #d3d3d3",
+    color: "#000",
+    overflow: "hidden",
+    transform: "scale(1.3)",
+    fontFamily: "Poppins, sans-serif",
+    fontSize: "10px",
+  },
+  watermark: {
+    position: "absolute",
+    top: "38%",
+    left: "1%",
+    transform: "scale(1.12)",
+    color: "#faf9f9",
+    fontSize: "90px",
+    fontWeight: "bold",
+    zIndex: "-1",
+  },
+  jobTitle: {
+    margin: "12px",
+    fontSize: "13px",
+    lineHeight: "15px",
+    color: "rgba(0, 0, 0, 0.5)",
+  },
+});
 
 export default function JobCard() {
-  const WaterMark = () => {
-    return <span className="watermark">Yugam</span>;
+  const classes = jobCardStyles();
+
+  const JobTitle = () => {
+    return (
+      <Typography fontWeight={700} className={classes.jobTitle}>
+        {candidate.job_title}
+      </Typography>
+    );
   };
 
-  const Divider = () => {
-    return <img src={divider} alt="divider" style={{ margin: "10px", width: "280px" }} />;
-  };
+  // const Divider = () => {
+  //   return <img src={divider} alt="divider" style={{ margin: "0", width: "164px", transform: "rotate(90deg)" }} />;
+  // };
 
   return (
-    <div className="jobCard">
-      <WaterMark />
+    <Card className={classes.jobCard}>
       <CardHeader name={candidate.name} />
-      <p className="jobTitle">{candidate.job_title}</p>
+      <JobTitle />
       <ContactDiv email={candidate.email} mobile={candidate.mobile} />
       <ExperienceDiv
         experience={candidate.experience}
@@ -30,8 +72,7 @@ export default function JobCard() {
         passing_year={candidate.passing_year}
       />
       <SalaryDiv current_ctc={candidate.current_ctc} current_inhand={candidate.current_inhand} expected_inhand={candidate.expected_inhand} />
-      <Divider />
       <MessageDiv />
-    </div>
+    </Card>
   );
 }
